acf250bcfb1c78993e0e33298a11d4452424f08a
Parakram <89983841+Parakramk@users.noreply.github.com>
2023-12-18 18:55:57 +0545

    Update Workflows to check update first

---
+++ commit.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+VERSION_FILE="commit.txt"
+
+update_version_file() {
+    echo "Updating version file with the latest version: $1"
+    echo "$1" > "$VERSION_FILE"
+    curl -s -X POST "https://api.telegram.org/bot$BOT_TOKEN/sendMessage" -F chat_id="$CHAT_ID" -F message_thread_id="$TOPIC_ID" \
+                      -F text="New Rimgo Commit with Hash $1 "> /dev/null
+}
+
+commit_update(){
+    git config --local user.name "Github Action"
+    git config --local user.email "action@github.com"
+    git add --all
+    git commit -m "Commit hash: $1"
+    git push
+}
+
+# Read the current commit from the file
+current_commit=$(<"$VERSION_FILE")
+
+
+json_response=$(curl -X 'GET' \
+  'https://codeberg.org/api/v1/repos/rimgo/rimgo/commits?page=1&limit=1' \
+  -H 'accept: application/json')
+
+latest_commit=$(echo "$json_response" | jq -r '.[0].sha')
+
+# Compare commit hash
+if [ "$latest_commit" != "$current_commit" ]; then
+    update_version_file "$latest_commit"
+    commit_update "$latest_commit"
+fi
+
+echo "Script execution completed."
